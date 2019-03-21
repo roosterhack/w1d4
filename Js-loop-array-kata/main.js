57d6f2951a603f7decd7ca2f11ced1e706ac9d98
Eddie Chung <designered@gmail.com>
2019-03-21 12:06:20 +0100

    solved challenge with loop

---
+++ Js-loop-array-kata/main.js
@@ -0,0 +1,20 @@
+// Goal: Extract from a two dimensional array (with the same height and width), concat all the elements from the "second diagonal", starting from the top right corner to the bottom left corner
+function concatSecondDiagonal(twoDimArray) {
+  let word = [];
+  for (let i = 0; i < twoDimArray.length; i++) {
+    word.push(`${twoDimArray[i][twoDimArray.length - 1 - i]}`);
+  }
+  return word.join("");
+}
+
+console.log(
+  concatSecondDiagonal([
+    ["a", "z", "x", "C", "v", "B"],
+    ["t", "w", "I", "p", "e", "7"],
+    ["w", "y", "o", "r", "m", "x"],
+    ["q", "U", "l", "s", "k", "H"],
+    ["A", "i", "G", "Y", "h", "L"],
+    ["n", "f", "d", "W", "z", "a"]
+  ])
+);
+// ==> "Berlin"
