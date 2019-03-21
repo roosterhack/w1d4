e0c9cdb46474350685a7640b503fceb3c2983f7a
Eddie Chung <designered@gmail.com>
2019-03-21 14:41:47 +0100

    added map solution

--- Js-loop-array-kata/main.js
+++ Js-loop-array-kata/main.js
@@ -1,10 +1,19 @@
 // Goal: Extract from a two dimensional array (with the same height and width), concat all the elements from the "second diagonal", starting from the top right corner to the bottom left corner
+// function concatSecondDiagonal(twoDimArray) {
+//   let word = [];
+//   for (let i = 0; i < twoDimArray.length; i++) {
+//     word.push(`${twoDimArray[i][twoDimArray.length - 1 - i]}`);
+//   }
+//   return word.join("");
+// }
+
+//Map solution
+//Map solution
 function concatSecondDiagonal(twoDimArray) {
-  let word = [];
-  for (let i = 0; i < twoDimArray.length; i++) {
-    word.push(`${twoDimArray[i][twoDimArray.length - 1 - i]}`);
-  }
-  return word.join("");
+  const charArray = twoDimArray.map((el, index, array) => {
+    return el[array.length - 1 - index];
+  });
+  return charArray.join("");
 }
 
 console.log(
